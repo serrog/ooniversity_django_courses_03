682754ba36b4dc90316560f58d500437eb2c9c3a
serrog <roganin.s@gmail.com>
2015-11-25 18:56:41 +0200

    KZ_5_1_bugfix1

--- students/urls.py
+++ students/urls.py
@@ -4,5 +4,5 @@ from students import views
 
 urlpatterns = patterns('',
     url(r'^(?P<student_id>\d+)/$', views.detail, name='detail'),
-    url(r'^/$', views.list_view, name='list_view'),
+    url(r'^$', views.list_view, name='list_view'),
 )
