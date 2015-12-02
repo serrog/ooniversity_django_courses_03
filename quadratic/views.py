10020df4a15ec726a7f7e9dd14fb987db4816896
serrog <roganin.s@gmail.com>
2015-12-02 21:58:10 +0200

    kz_7_2_bugfix1

--- quadratic/views.py
+++ quadratic/views.py
@@ -8,20 +8,22 @@ def quadratic_results(request):
         b = request.GET.get('b')
         c = request.GET.get('c')
         cont = {}
-        print a
         if a == None and b == None and c == None:
             form = forms.QuadraticForm()
         else:
             form = forms.QuadraticForm(request.GET)
         if form.is_valid():
-            d = int(b) * int(b) - 4 * int(a) * int(c)
+            a_f = form.cleaned_data['a']
+            b_f = form.cleaned_data['b']
+            c_f = form.cleaned_data['c']
+            d = b_f * b_f - 4 * a_f * c_f
             cont['d'] = "Дискриминант: %(d)d" % { 'd':d }
             if d == 0:
-                x = -int(b) / (2.0 * int(a))
+                x = -b_f / (2.0 * a_f)
                 cont['result'] = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %(x).1f" % {'x' : x}
             elif d > 0:
-                x1 = (-int(b) + d ** (1 / 2.0)) / (2.0 * int(a))
-                x2 = (-int(b) - d ** (1 / 2.0)) / (2.0 * int(a))
+                x1 = (-b_f + d ** (1 / 2.0)) / (2.0 * a_f)
+                x2 = (-b_f - d ** (1 / 2.0)) / (2.0 * a_f)
                 cont['result'] = "Квадратное уравнение имеет два действительных корня: x1 = %(x1).1f, x2 = %(x2).1f" % {'x1':x1, 'x2':x2 }
             else:
                 cont['result'] = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений." 
