b78ed4724891ea2d2bd2e5a7409df70bdbda3103
serrog <roganin.s@gmail.com>
2015-12-16 07:39:55 +0200

    KZ_11_2

--- courses/tests.py
+++ courses/tests.py
@@ -1,32 +1,163 @@
+from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
 from django.test import Client
+from datetime import date
+from django.contrib.auth.models import User
 from courses.models import Course
+from coaches.models import Coach
+from pybursa.views import index
+from courses.views import CourseDetailView
+
+def create_courses():
+    user1 = User.objects.create(
+        username = 'user1'
+    )
+    user2 = User.objects.create(
+        username = 'user2'
+    )
+    coach1 = Coach.objects.create(
+        user=user1, 
+        date_of_birth=date.today(),
+        gender='M',
+        phone='123456',
+        address='test address 1',
+        skype='test skype 1',
+        description='description 1'
+    )
+    coach2 = Coach.objects.create(
+        user=user2, 
+        date_of_birth=date.today(),
+        gender='F',
+        phone='1234567',
+        address='test address 2',
+        skype='test skype 2',
+        description='description 2'
+    )
+    course1 = Course.objects.create(
+        name='course1', 
+        short_description='short description 1',
+        description='description 1',
+        coach=coach1,
+        assistant=coach2
+    )
+    course2 = Course.objects.create(
+        name='course2', 
+        short_description='short description 2',
+        description='description 2',
+        coach=coach1,
+        assistant=coach2
+    )
+    
+
 
 class CoursesListTest(TestCase):
 
-	def test_index_page_without_courses(self):
-		client = Client()
-		response = client.get('/')
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'index.html')
+    def test_root_url_resolves_to_index_page(self):
+        self.assertEqual(resolve('/').func, index)
 
-	def test_index_page_with_courses(self):
-		client = Client()
-		course1 = Course.create(
-			'name': 'course1', 
-			'short_description': 'short description 1',
-			'description': 'description 1'
-		)
-		course2 = Course.create(
-			'name': 'course2', 
-			'short_description': 'short description 2',
-			'description': 'description 2'
-		)
-		response = client.get('/')
-		self.assertEqual(response.status_code, 200)
-		self.assertTemplateUsed(response, 'index.html')
-		self.assertEqual(len(response.context['all_courses']), 2)
+    def test_index_view_render_wright_template(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_rendered_correct_html(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, '<h2>Ooniversity</h2>')
+        self.assertContains(response, '<h3>Join our courses!</h3>')
+
+    def test_index_response_contains_correct_context(self):
+        client = Client()
+        create_courses()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['all_courses']), 2)
+        for i, course in enumerate(response.context['all_courses']):
+            self.assertEqual(course.name, 'course{}'.format(i+1))
+            self.assertEqual(course.short_description, 'short description {}'.format(i+1))
+            self.assertEqual(course.description, 'description {}'.format(i+1))
+
+    def test_index_template_contains_correct_links_to_edit_course(self):
+        client = Client()
+        create_courses()
+        response = client.get('/')
+        for i in range(1, 3):
+            self.assertContains(response, 'href="/courses/edit/{}/"'.format(i))
+
+    def test_index_template_contains_correct_links_to_delete_course(self):
+        client = Client()
+        create_courses()
+        response = client.get('/')
+        for i in range(1, 3):
+            self.assertContains(response, 'href="/courses/remove/{}/"'.format(i))
+
+    def test_index_template_contains_correct_links_to_add_course(self):
+        client = Client()
+        response = client.get('/')
+        self.assertContains(response, 'href="/courses/add/"')
 
 
-		assertContains(response, text, count=None, status_code=200, msg_prefix='', html=False)
 
+class CoursesDetailTest(TestCase):
+
+    def test_detail_url_resolves_to_detail_page(self):
+        create_courses()
+        for i in range(1, 3):
+            self.assertEqual(
+                resolve('/courses/{}/'.format(i)).func.__name__, 
+                CourseDetailView.as_view().__name__
+            )
+
+    def test_detail_view_not_render_wright_template_with_non_existing_course(self):
+        client = Client()
+        response = client.get(reverse('courses:detail', args=(1, )))
+        self.assertEqual(response.status_code, 404)
+
+    def test_index_view_render_wright_template(self):
+        create_courses()
+        client = Client()
+        for i in range(1, 3):
+            response = client.get(reverse('courses:detail', args=(i,)))
+            self.assertEqual(response.status_code, 200)
+
+    def test_rendered_correct_html(self):
+        client = Client()
+        create_courses()
+        response = client.get(reverse('courses:detail', args=(1,)))
+        self.assertTemplateUsed(response, 'courses/detail.html')
+        self.assertContains(response, '<h3>Coaches</h3>')
+
+    def test_course_detail_response_contains_correct_context(self):
+        client = Client()
+        create_courses()
+        for i in range(1, 3):
+            response = client.get(reverse('courses:detail', args=(i,)))
+            course = response.context['course_detail']
+            self.assertEqual(course.name, 'course{}'.format(i))
+            self.assertEqual(course.short_description, 'short description {}'.format(i))
+            self.assertEqual(course.description, 'description {}'.format(i))
+
+    def test_course_detail_template_contains_link_to_add_lesson(self):
+        client = Client()
+        create_courses()
+        for i in range(1, 3):
+            response = client.get(reverse('courses:detail', args=(i,)))
+            self.assertContains(response, 'href="/courses/{}/add_lesson"'.format(i))
+
+    def test_course_detail_template_contains_link_to_coach(self):
+        client = Client()
+        create_courses()
+        for i in range(1, 3):
+            response = client.get(reverse('courses:detail', args=(i,)))
+            self.assertContains(response, 'href="/coaches/1/"')
+
+    def test_course_detail_template_contains_link_to_assistant(self):
+        client = Client()
+        create_courses()
+        for i in range(1, 3):
+            response = client.get(reverse('courses:detail', args=(i,)))
+            self.assertContains(response, 'href="/coaches/2/"')
+
+
+       
--- students/tests.py
+++ students/tests.py
@@ -1,3 +1,172 @@
+from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
+from datetime import date
+from django.test import Client
+from students.models import Student
+from courses.models import Course
+from students.views import StudentListView, StudentDetailView
 
-# Create your tests here.
+def create_students():
+    course1 = Course.objects.create(
+        name = 'course1', 
+        short_description = 'short description 1',
+        description = 'description 1'
+    )
+    course2 = Course.objects.create(
+        name = 'course2', 
+        short_description = 'short description 2',
+        description = 'description 2'
+    )
+    course3 = Course.objects.create(
+        name = 'course3', 
+        short_description = 'short description 3',
+        description = 'description 3'
+    )
+    students1 = Student.objects.create(
+        name = 'Name1',
+        surname = 'Surname1',
+        date_of_birth = date.today(),
+        email = 'email1@ex.com',
+        phone = '123456',
+        address = 'Adress1',
+        skype = 'Skype1'
+    )
+    students1.courses.add(course1)
+    students2 = Student.objects.create(
+        name = 'Name2',
+        surname = 'Surname2',
+        date_of_birth = date.today(),
+        email = 'email2@ex.com',
+        phone = '123456',
+        address = 'Adress2',
+        skype = 'Skype2'
+    )
+    students2.courses.add(course3)
+    students3 = Student.objects.create(
+        name = 'Name3',
+        surname = 'Surname3',
+        date_of_birth = date.today(),
+        email = 'email3@ex.com',
+        phone = '123456',
+        address = 'Adress3',
+        skype = 'Skype3'
+    )
+    students3.courses.add(course3)
+
+
+class StudentsListTest(TestCase):
+
+    def test_student_list_url_resolves_to_list_page(self):
+        self.assertEqual(resolve(
+            '/students/').func.__name__, 
+            StudentListView.as_view().__name__
+        )
+
+    def test_student_list_view_render_wright_template(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'students/student_list.html')
+
+    def test_rendered_correct_html(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, "<h2>Student's list</h2>")
+        self.assertContains(response, '<th>Name</th>')
+        self.assertContains(response, '<th>Name</th>')
+        self.assertContains(response, '<th>Address</th>')
+        self.assertContains(response, '<th>Skype</th>')
+
+
+    def test_student_list_response_contains_correct_context(self):
+        client = Client()
+        create_students()
+        response = client.get('/students/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['object_list']), 2)
+        for i, student in enumerate(response.context['object_list']):
+            self.assertEqual(student.name, 'Name{}'.format(i+1))
+            self.assertEqual(student.surname, 'Surname{}'.format(i+1))
+            self.assertEqual(student.email, 'email{}@ex.com'.format(i+1))
+            self.assertEqual(student.skype, 'Skype{}'.format(i+1))
+
+    def test_student_list_response_contains_correct_context_for_one_course(self):
+        client = Client()
+        create_students()
+        response = client.get('/students/?course_id=3')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['object_list']), 2)
+        for i, student in enumerate(response.context['object_list']):
+            self.assertEqual(student.name, 'Name{}'.format(i+2))
+            self.assertEqual(student.surname, 'Surname{}'.format(i+2))
+            self.assertEqual(student.email, 'email{}@ex.com'.format(i+2))
+            self.assertEqual(student.skype, 'Skype{}'.format(i+2))
+
+    def test_student_list_template_contains_correct_links_to_edit_student(self):
+        client = Client()
+        create_students()
+        response = client.get('/students/')
+        for i in range(1, 3):
+            self.assertContains(response, 'href="/students/edit/{}/"'.format(i))
+
+    def test_student_list_template_contains_correct_links_to_delete_students(self):
+        client = Client()
+        create_students()
+        response = client.get('/students/')
+        for i in range(1, 3):
+            self.assertContains(response, 'href="/students/remove/{}/"'.format(i))
+
+    def test_student_list_template_contains_correct_links_to_add_student(self):
+        client = Client()
+        response = client.get('/students/')
+        self.assertContains(response, 'href="/students/add/"')
+
+
+
+class StudentsDetailTest(TestCase):
+
+    def test_detail_url_resolves_to_detail_page(self):
+        create_students()
+        for i in range(1, 4):
+            self.assertEqual(
+                resolve('/students/{}/'.format(i)).func.__name__, 
+                StudentDetailView.as_view().__name__
+            )
+
+    def test_detail_view_not_render_wright_template_with_non_existing_student(self):
+        client = Client()
+        response = client.get(reverse('students:detail', args=(1, )))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_render_wright_template(self):
+        create_students()
+        client = Client()
+        for i in range(1, 4):
+            response = client.get(reverse('students:detail', args=(i,)))
+            self.assertEqual(response.status_code, 200)
+
+    def test_rendered_correct_html(self):
+        client = Client()
+        create_students()
+        response = client.get(reverse('students:detail', args=(1,)))
+        self.assertTemplateUsed(response, 'students/student_detail.html')
+        self.assertContains(response, '<h2>Name1 Surname1</h2>')
+
+    def test_student_detail_response_contains_correct_context(self):
+        client = Client()
+        create_students()
+        for i in range(1, 4):
+            response = client.get(reverse('students:detail', args=(i,)))
+            student = response.context['object']
+            self.assertEqual(student.name, 'Name{}'.format(i))
+            self.assertEqual(student.surname, 'Surname{}'.format(i))
+            self.assertEqual(student.email, 'email{}@ex.com'.format(i))
+            self.assertEqual(student.skype, 'Skype{}'.format(i))
+
+    def test_student_detail_template_contains_link_to_courses(self):
+        client = Client()
+        create_students()
+        response = client.get(reverse('students:detail', args=(1,)))
+        self.assertContains(response, 'href="/courses/1/"')
+
+    
